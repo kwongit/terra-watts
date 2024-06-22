ba4c5156b4590fe557e9e9f14fb9419c9338ade7
kevin <kevinwon30@gmail.com>
2024-06-22 08:04:21 -0700

    updated css

--- assets/js/transform3d.js
+++ assets/js/transform3d.js
@@ -1,7 +1,7 @@
 document.querySelectorAll(".image-transform").forEach((element) => {
   const image = element.querySelector("img");
 
-  element.addEventListener("mousemove", (event) => {
+  function handleMouseMove(event) {
     const rect = element.getBoundingClientRect();
     const x = event.clientX - rect.left; // x position within the element.
     const y = event.clientY - rect.top; // y position within the element.
@@ -16,9 +16,25 @@ document.querySelectorAll(".image-transform").forEach((element) => {
     const rotateX = -percentY * 15; // Adjust the rotation angle as needed
 
     image.style.transform = `rotateY(${rotateY}deg) rotateX(${rotateX}deg)`;
-  });
+  }
 
-  element.addEventListener("mouseleave", () => {
+  function handleMouseLeave() {
     image.style.transform = `rotateY(0deg) rotateX(0deg)`;
-  });
+  }
+
+  function updateTransformEffect() {
+    if (window.matchMedia("(orientation: landscape)").matches) {
+      element.addEventListener("mousemove", handleMouseMove);
+      element.addEventListener("mouseleave", handleMouseLeave);
+    } else {
+      element.removeEventListener("mousemove", handleMouseMove);
+      element.removeEventListener("mouseleave", handleMouseLeave);
+    }
+  }
+
+  // Initial check
+  updateTransformEffect();
+
+  // Update on orientation change
+  window.addEventListener("orientationchange", updateTransformEffect);
 });
