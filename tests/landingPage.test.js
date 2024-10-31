989b3ec2897b8293eca29f9f4b6eddc4fe9f96e7
kevin <kevinwon30@gmail.com>
2024-10-31 16:26:04 -0700

    Added comments

--- tests/landingPage.test.js
+++ tests/landingPage.test.js
@@ -1,15 +1,14 @@
 import { test, expect } from "@playwright/test";
 
 test("Landing Page should load successfully", async ({ page }) => {
+  // Use 'page' to interact with the web app, e.g., navigating to a URL
   await page.goto("https://terra-watts.com/");
 
-  // Preferred way to check title, as it waits for the title to be available before checking it (unlike page.title()) - https://playwright.dev/docs/api/class-page#pagewaitfortitle
+  // Use 'expect' to assert conditions about the page state, e.g., check the title. This is the preferred way to check title, as it waits for the title to be available before checking it (unlike page.title()) - https://playwright.dev/docs/api/class-page#pagewaitfortitle
   await expect(page).toHaveTitle("Terra Watts - Renewable Energy Solutions");
-  // expect(await page.title()).toBe("Terra Watts - Renewable Energy Solutions");
 
   // TODO: Add more tests here
   // Check for description meta tag content - https://playwright.dev/docs/api/class-locator#locatorgetattribute
-
   // const description = await page
   //   .locator('meta[name="description"]')
   //   .getAttribute("content");
