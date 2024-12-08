d7f15fc343eead3c6fb998a49f2db8e7ca9e9e0a
kevin <kevinwon30@gmail.com>
2024-12-07 21:31:18 -0800

    Refactored test data

---
+++ tests/data/expectedSpotlightContent.js
@@ -0,0 +1,18 @@
+// Expected content for headings and paragraphs
+export const expectedSpotlightContent = [
+  {
+    heading: "Critical Need",
+    paragraph:
+      "The demand for minerals vital to energy technologies is projected to surge by 500 percent by 2050, posing a significant challenge to combating climate change and ensuring national security. Geoscientists have expressed concerns about the limited resources available to meet the demand. It is therefore crucial to develop innovative approaches for the transition toward clean energy. One potential solution is to reduce our dependence on minerals by developing wireless methods to transmit power and electrify devices.",
+  },
+  {
+    heading: "Technology Vision",
+    paragraph:
+      "Innovation in wireless power transmission can potentially reduce dependency on wires and batteries. However, current wireless power technology faces several limitations, including line-of-sight dependence and short transmission range. Terra Watts is revolutionizing the industry by introducing technology that uses the earth as a power propagation medium, enabling wireless power transmission at the kilometer scale. This technology allows for the direct operation of electrical devices via a receiver—no batteries required. Moreover, Terra Watts is innovating in underground data communication methods, an emerging technology that can uniquely facilitate simultaneous long-range wireless power transmission and data communication.",
+  },
+  {
+    heading: "Potential For Impact",
+    paragraph:
+      "Terra Watt’s innovation in wireless power transmission has versatile applications across many industries. The near-term application will significantly impact farming by wirelessly powering and transmitting data to underground soil sensors. The long-term goal is to deliver power to larger electronic devices and buildings in agriculture and other industries. Moreover, advancements in wireless power transmission can increase the range of the energy grid by eliminating the need for wires, aiding in the challenges of delivering power to the last mile. The potential of wireless power transmission is enormous, and its development is crucial for a sustainable future.",
+  },
+];
